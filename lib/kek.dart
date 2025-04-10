73544e624896ec76814858c02f6a5de877967c21
solid-tetianamovlian <tetiana.movlian@solid.software>
2025-04-10 16:56:36 +0300

    Implement Kek widget

---
+++ lib/kek.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Kek extends StatelessWidget {
+  const Kek({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
